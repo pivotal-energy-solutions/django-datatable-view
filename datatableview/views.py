70f5194e51dc0ed9c7e2a456773b52b9effb544a
Tim Valenta <tonightslastsong@gmail.com>
2012-12-04 13:18:42 -0700

    Fixed issue where relationship-spanning lookups failed
    Instead of returning Field subclasses, error conditions rose
    AttributeErrors during lookup when objects such as
    SingleRelatedObjectDescriptor were accessed instead of the target model
    class itself.

--- datatableview/views.py
+++ datatableview/views.py
@@ -139,6 +139,16 @@ class DatatableMixin(MultipleObjectMixin):
             queries = [] # Queries generated to search all fields for all terms
             search_terms = map(unicode.strip, options.search.split())
             
+            def field_getter(item, bit):
+                try:
+                    item = item.related.model
+                except AttributeError:
+                    try:
+                        item = item.field.rel.to
+                    except AttributeError:
+                        pass
+                return getattr(item, bit)
+            
             for term in search_terms:
                 term_queries = [] # Queries generated to search all fields for this term
                 # Every concrete database lookup string in 'columns' is followed to its trailing field descriptor.  For example, "subdivision__name" terminates in a CharField.  The field type determines how it is probed for search.
@@ -151,10 +161,13 @@ class DatatableMixin(MultipleObjectMixin):
                     for component_name in name:
                         field_queries = [] # Queries generated to search this database field for the search term
                         bits = component_name.split('__')
-                        obj = reduce(getattr, [self.model] + bits[:-1])
+                        obj = reduce(field_getter, [self.model] + bits[:-1])
                         
                         if obj is not self.model:
-                            obj = obj.field.rel.to
+                            if hasattr(obj, 'related'): # one of the object descriptors
+                                obj = obj.related.model
+                            else: # one of the foreign key fields
+                                obj = obj.field.rel.to
                             
                         # Get the Field type from the related model
                         try:
