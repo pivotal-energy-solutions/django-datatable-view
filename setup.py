9e568542229874f701baada276f0aeae28151c43
Steven Klass <sklass@pointcircle.com>
2012-12-05 14:02:15 -0700

    Hopefully 12 is charm.. Fuck.

--- setup.py
+++ setup.py
@@ -2,6 +2,41 @@
 """setup.py: Django django-datatables-view"""
 
 from distutils.core import setup
+import os
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+_dir = 'datatableview'
+
+for dirpath, dirnames, filenames in os.walk(_dir):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.') or dirname == '__pycache__':
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
 
 setup(name='django-datatable-view',
       version='1.0',
@@ -24,6 +59,7 @@ setup(name='django-datatable-view',
            'Programming Language :: Python',
            'Topic :: Software Development',
       ],
-      packages=['datatableview'],
+      packages=packages,
+      data_files=data_files,
       requires=['django (>=1.2)',],
 )
