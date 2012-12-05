89bbf75510607f4d1597f78eaf3895bbaa7c3d76
Steven Klass <sklass@pointcircle.com>
2012-12-05 14:45:09 -0700

    needed this so we look here for templates..

---
+++ datatableview/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-"""urls.py: Django datatableview"""
+
+import logging
+
+__author__ = 'Steven Klass'
+__date__ = '12/5/12 2:44 PM'
+__copyright__ = 'Copyright 2012 7Stalks Consulting. All rights reserved.'
+__credits__ = ['Steven Klass', ]
+
+log = logging.getLogger(__name__)
