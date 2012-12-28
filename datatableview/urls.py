17ed149a9b195449135455c9053e546d6643b56b
Steven Klass <sklass@7stalks.com>
2012-12-28 10:03:53 -0700

    Updated the licensing dates to reflect multi-year.

--- datatableview/urls.py
+++ datatableview/urls.py
@@ -4,7 +4,7 @@ import logging
 
 __author__ = 'Steven Klass'
 __date__ = '12/5/12 2:44 PM'
-__copyright__ = 'Copyright 2012 7Stalks Consulting. All rights reserved.'
+__copyright__ = 'Copyright 2012-2013 Pivotal Energy Solutions. All rights reserved.'
 __credits__ = ['Steven Klass', ]
 
 log = logging.getLogger(__name__)
