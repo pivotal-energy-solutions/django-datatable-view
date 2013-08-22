464a42dcdc86985831dff9546e34e400cedbffb2
Tim Valenta <tonightslastsong@gmail.com>
2013-08-22 14:42:20 -0700

    Remove use of removed decorator mechanism

--- datatableview/views.py
+++ datatableview/views.py
@@ -466,9 +466,6 @@ class DatatableMixin(MultipleObjectMixin):
                 # Method name is explicitly given
                 method_name = name[2]
                 if callable(method_name):
-                    if hasattr(method_name, '_is_wrapped'):
-                        # Wrapped by keyed_helper, but hasn't received a key argument
-                        return True, method_name()
                     return True, method_name
                 return True, getattr(self, method_name)
 
