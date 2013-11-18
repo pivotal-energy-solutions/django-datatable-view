256efe32d904568a8d5a0df6d15db4b6e21278c2
Tim Valenta <tonightslastsong@gmail.com>
2013-11-18 09:58:40 -0700

    Fix contributor credits
    This was a copy-paste error from a package I was using as a model.

--- datatableview/__init__.py
+++ datatableview/__init__.py
@@ -4,5 +4,5 @@ __author__ = 'Tim Valenta'
 __version_info__ = (0, 5, 0)
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '2013/11/14 2:00:00 PM'
-__credits__ = ['Steven Klass', 'Marty Alchin', 'George Vilches', 'Corey Betram']
+__credits__ = ['Tim Valenta', 'Steven Klass']
 __license__ = 'See the file LICENSE.txt for licensing information.'
