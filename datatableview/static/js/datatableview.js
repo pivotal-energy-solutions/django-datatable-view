094774b8594bcb5e6a1267580480fef91c31302b
Tim Valenta <tonightslastsong@gmail.com>
2013-12-03 14:48:47 -0700

    Add JS make_xeditable callback

--- datatableview/static/js/datatableview.js
+++ datatableview/static/js/datatableview.js
@@ -1,4 +1,37 @@
 /* For datatable view */
+
+var datatableview = {
+	make_xeditable: function(nRow, mData, iDisplayIndex) {
+	    $('td a[data-xeditable]', nRow).editable({
+			ajaxOptions: {
+				headers: { 'X-CSRFToken': datatableview.getCookie('csrftoken') }
+			},
+	        success: function (response, newValue) {
+	            if (response.status == 'error') {
+					return response.msg;
+				}
+	        },
+	    });
+		return nRow;
+	},
+
+	getCookie: function(name) {
+	    var cookieValue = null;
+	    if (document.cookie && document.cookie != '') {
+	        var cookies = document.cookie.split(';');
+	        for (var i = 0; i < cookies.length; i++) {
+	            var cookie = jQuery.trim(cookies[i]);
+	            // Does this cookie string begin with the name we want?
+	            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+	                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+	                break;
+	            }
+	        }
+	    }
+	    return cookieValue;
+	}
+}
+
 $(function(){
 	if (!window.console) {
 		console = {
