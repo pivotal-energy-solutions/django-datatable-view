e8d9d40197d718541290be30d7bf686ef87eb84b
Tim Valenta <tonightslastsong@gmail.com>
2013-12-06 18:48:14 -0700

    Add DatatableOptions validation tests

--- datatableview/tests/test_utils.py
+++ datatableview/tests/test_utils.py
@@ -171,3 +171,121 @@ class UtilsTests(DatatableViewTestCase):
         ]
         structure = get_structure(columns, {})
         self.assertEqual(len(list(structure)), len(columns))
+
+    def test_options_use_default_to_local_fields(self):
+        """ Verifies that no columns specified in options means showing all local fields. """
+        opts = {}
+        options = utils.DatatableOptions(models.ExampleModel, {}, **opts)
+        local_field_names = [f.verbose_name.capitalize() for f in models.ExampleModel._meta.local_fields]
+        self.assertEqual(options.columns, local_field_names)
+
+    def test_options_use_defaults(self):
+        """ Verifies that no options normalizes to the default set. """
+        options = utils.DatatableOptions(models.ExampleModel, {})
+        self.assertEqual(options, dict(utils.DEFAULT_OPTIONS, columns=options.columns))
+
+    def test_options_normalize_values(self):
+        """ Verifies that the options object fixes bad values. """
+        model = models.ExampleModel
+        opts = {
+            'search_fields': None,
+            'unsortable_columns': None,
+            'hidden_columns': None,
+        }
+        options = utils.DatatableOptions(model, {}, **opts)
+        self.assertEqual(options.search_fields, [])
+        self.assertEqual(options.unsortable_columns, [])
+        self.assertEqual(options.hidden_columns, [])
+
+        data = { utils.OPTION_NAME_MAP['start_offset']: -5 }
+        options = utils.DatatableOptions(model, data)
+        self.assertEqual(options.start_offset, 0)
+        data = { utils.OPTION_NAME_MAP['start_offset']: 'not a number' }
+        options = utils.DatatableOptions(model, data)
+        self.assertEqual(options.start_offset, 0)
+
+        data = { utils.OPTION_NAME_MAP['page_length']: -5 }
+        options = utils.DatatableOptions(model, data)
+        self.assertEqual(options.page_length, utils.MINIMUM_PAGE_LENGTH)
+        data = { utils.OPTION_NAME_MAP['page_length']: -1 }  # special case for dataTables.js
+        options = utils.DatatableOptions(model, data)
+        self.assertEqual(options.page_length, -1)
+        data = { utils.OPTION_NAME_MAP['page_length']: 'not a number' }
+        options = utils.DatatableOptions(model, data)
+        self.assertEqual(options.page_length, utils.DEFAULT_OPTIONS['page_length'])
+
+    def test_options_sorting_validation(self):
+        """ Verifies that sorting options respect configuration. """
+        model = models.ExampleModel
+        opts = {
+            'columns': [
+                'id',
+                'date_created',
+                'name',
+            ],
+            'ordering': ['name', 'id'],
+            'unsortable_columns': ['date_created'],
+        }
+
+        # Invalid sort number means use default sorting
+        data = { utils.OPTION_NAME_MAP['num_sorting_columns']: 'not a number' }
+        options = utils.DatatableOptions(model, data, **opts)
+        self.assertEqual(options.ordering, ['name', 'id'])
+
+        # Invalid sort index means no sorting for that sorting priority
+        data = {
+            utils.OPTION_NAME_MAP['num_sorting_columns']: '2',
+            (utils.OPTION_NAME_MAP['sort_column'] % 0): '999',  # bad column index
+            (utils.OPTION_NAME_MAP['sort_column_direction'] % 0): 'asc',
+            (utils.OPTION_NAME_MAP['sort_column'] % 1): '2',
+            (utils.OPTION_NAME_MAP['sort_column_direction'] % 1): 'asc',
+        }
+        options = utils.DatatableOptions(model, data, **opts)
+        self.assertEqual(options.ordering, ['name'])
+
+        # Sort requested for unsortable column rejects sorting
+        data = {
+            utils.OPTION_NAME_MAP['num_sorting_columns']: '2',
+            (utils.OPTION_NAME_MAP['sort_column'] % 0): '1',  # unsortable column index
+            (utils.OPTION_NAME_MAP['sort_column_direction'] % 0): 'asc',
+            (utils.OPTION_NAME_MAP['sort_column'] % 1): '0',
+            (utils.OPTION_NAME_MAP['sort_column_direction'] % 1): 'asc',
+        }
+        options = utils.DatatableOptions(model, data, **opts)
+        self.assertEqual(options.ordering, ['id'])
+
+        # Invalid sort direction rejects sorting
+        data = {
+            utils.OPTION_NAME_MAP['num_sorting_columns']: '2',
+            (utils.OPTION_NAME_MAP['sort_column'] % 0): '2',
+            (utils.OPTION_NAME_MAP['sort_column_direction'] % 0): 'bad direction',  # unusable value
+            (utils.OPTION_NAME_MAP['sort_column'] % 1): '0',
+            (utils.OPTION_NAME_MAP['sort_column_direction'] % 1): 'asc',
+        }
+        options = utils.DatatableOptions(model, data, **opts)
+        self.assertEqual(options.ordering, ['id'])
+
+    def test_options_normalize_virtual_columns_to_special_names(self):
+        """
+        Verifies that virtual field receives index-based synonym to denote special conditions that
+        will affect sorting/searching behavior.
+        """
+        model = models.ExampleModel
+        opts = {
+            'columns': [
+                'id',
+                'pk',  # 'pk' is technically not a real field, so it registers as fake!
+                'fake',
+            ],
+        }
+        data = {
+            utils.OPTION_NAME_MAP['num_sorting_columns']: '3',
+            (utils.OPTION_NAME_MAP['sort_column'] % 0): '0',
+            (utils.OPTION_NAME_MAP['sort_column_direction'] % 0): 'desc',
+            (utils.OPTION_NAME_MAP['sort_column'] % 1): '1',
+            (utils.OPTION_NAME_MAP['sort_column_direction'] % 1): 'desc',
+            (utils.OPTION_NAME_MAP['sort_column'] % 2): '2',
+            (utils.OPTION_NAME_MAP['sort_column_direction'] % 2): 'asc',
+        }
+        options = utils.DatatableOptions(model, data, **opts)
+        self.assertEqual(options.ordering, ['-id', '-!1', '!2'])
