b64a17399126a28ddb50fbbfcf17d4b0f5484f55
Tim Valenta <tonightslastsong@gmail.com>
2013-12-09 14:26:30 -0700

    Add explicit initialization utility to javascript

--- datatableview/static/js/datatableview.js
+++ datatableview/static/js/datatableview.js
@@ -1,6 +1,7 @@
 /* For datatable view */
 
 var datatableview = {
+    auto_initialize: true,
     make_xeditable: function(options) {
         var options = $.extend({}, options);
         if (!options.ajaxOptions) {
@@ -35,99 +36,105 @@ var datatableview = {
             }
         }
         return cookieValue;
+    },
+
+    initialize: function($$) {
+        if (typeof window.console === "undefined" || typeof window.console.log === "undefined") {
+            console = {
+                log: function(){},
+                info: function(){}
+            };
+        }
+        var options_name_map = {
+            'sortable': 'bSortable',
+            'sorting': 'aaSorting',
+            'visible': 'bVisible'
+        };
+
+        var template_clear_button = $('<a href="#" class="clear-search">Clear</a>');
+
+        $$.each(function(){
+            var datatable = $(this);
+            var column_options = [];
+            var sorting_options = [];
+
+            datatable.find('thead th').each(function(){
+                var header = $(this);
+                var options = {};
+                for (var i = 0; i < header[0].attributes.length; i++) {
+                    var attr = header[0].attributes[i];
+                    if (attr.specified && /^data-/.test(attr.name)) {
+                        var name = attr.name.replace(/^data-/, '');
+                        var value = attr.value;
+
+                        // Typecasting out of string
+                        name = options_name_map[name];
+                        if (/^b/.test(name)) {
+                            value = (value === 'true');
+                        }
+
+                        if (name == 'aaSorting') {
+                            // This doesn't go in the column_options
+                            var sort_info = value.split(',');
+                            sort_info[1] = parseInt(sort_info[1]);
+                            sorting_options.push(sort_info);
+                            continue;
+                        }
+
+                        options[name] = value;
+                    }
+                }
+                column_options.push(options);
+            });
+
+            // Arrange the sorting column requests and strip the priority information
+            sorting_options.sort(function(a, b){ return a[0] - b[0] });
+            for (var i = 0; i < sorting_options.length; i++) {
+                sorting_options[i] = sorting_options[i].slice(1);
+            }
+
+            options = {
+                "bServerSide": true,
+                "aaSorting": sorting_options,
+                "aoColumns": column_options,
+                "sAjaxSource": datatable.attr('data-source-url'),
+                "fnInfoCallback": function(oSettings, iStart, iEnd, iMax, iTotal, sPre){
+                    $("#" + datatable.attr('data-result-counter-id')).html(iTotal);
+                    var infoString = "Showing "+iStart +" to "+ iEnd+" of "+iTotal+" entries";
+                    if (iMax != iTotal) {
+                        infoString +=  " (filtered from "+iMax+" total entries)";
+                    }
+                    return infoString;
+                }
+            }
+            try {
+                options = confirm_datatable_options(options, datatable);
+            } catch (e) {
+
+            }
+
+            var initialized_datatable = datatable.dataTable(options);
+
+            try {
+                initialized_datatable.fnSetFilteringDelay();
+            } catch (e) {
+                console.info("datatable plugin fnSetFilteringDelay not available");
+            }
+
+            var search_input = initialized_datatable.closest('.dataTables_wrapper').find('.dataTables_filter input');
+            var clear_button = template_clear_button.clone().click(function(){
+                $(this).trigger('clear.datatable', [initialized_datatable]);
+                return false;
+            }).bind('clear.datatable', function(){
+                search_input.val('').keyup();
+            });
+            search_input.after(clear_button).after(' ');
+        });
     }
 }
 
 $(function(){
-    if (typeof window.console === "undefined" || typeof window.console.log === "undefined") {
-        console = {
-            log: function(){},
-            info: function(){}
-        };
+    if (datatableview.auto_initialize) {
+        datatableview.initialize($('.datatable'));
     }
-    var options_name_map = {
-        'sortable': 'bSortable',
-        'sorting': 'aaSorting',
-        'visible': 'bVisible'
-    };
-
-    var template_clear_button = $('<a href="#" class="clear-search">Clear</a>');
-
-    $('.datatable').each(function(){
-        var datatable = $(this);
-        var column_options = [];
-        var sorting_options = [];
-
-        datatable.find('thead th').each(function(){
-            var header = $(this);
-            var options = {};
-            for (var i = 0; i < header[0].attributes.length; i++) {
-                var attr = header[0].attributes[i];
-                if (attr.specified && /^data-/.test(attr.name)) {
-                    var name = attr.name.replace(/^data-/, '');
-                    var value = attr.value;
-
-                    // Typecasting out of string
-                    name = options_name_map[name];
-                    if (/^b/.test(name)) {
-                        value = (value === 'true');
-                    }
-
-                    if (name == 'aaSorting') {
-                        // This doesn't go in the column_options
-                        var sort_info = value.split(',');
-                        sort_info[1] = parseInt(sort_info[1]);
-                        sorting_options.push(sort_info);
-                        continue;
-                    }
-
-                    options[name] = value;
-                }
-            }
-            column_options.push(options);
-        });
-
-        // Arrange the sorting column requests and strip the priority information
-        sorting_options.sort(function(a, b){ return a[0] - b[0] });
-        for (var i = 0; i < sorting_options.length; i++) {
-            sorting_options[i] = sorting_options[i].slice(1);
-        }
-
-        options = {
-            "bServerSide": true,
-            "aaSorting": sorting_options,
-            "aoColumns": column_options,
-            "sAjaxSource": datatable.attr('data-source-url'),
-            "fnInfoCallback": function(oSettings, iStart, iEnd, iMax, iTotal, sPre){
-                $("#" + datatable.attr('data-result-counter-id')).html(iTotal);
-                var infoString = "Showing "+iStart +" to "+ iEnd+" of "+iTotal+" entries";
-                if (iMax != iTotal) {
-                    infoString +=  " (filtered from "+iMax+" total entries)";
-                }
-                return infoString;
-            }
-        }
-        try {
-            options = confirm_datatable_options(options, datatable);
-        } catch (e) {
-
-        }
-
-        var initialized_datatable = datatable.dataTable(options);
-
-        try {
-            initialized_datatable.fnSetFilteringDelay();
-        } catch (e) {
-            console.info("datatable plugin fnSetFilteringDelay not available");
-        }
-
-        var search_input = initialized_datatable.closest('.dataTables_wrapper').find('.dataTables_filter input');
-        var clear_button = template_clear_button.clone().click(function(){
-            $(this).trigger('clear.datatable', [initialized_datatable]);
-            return false;
-        }).bind('clear.datatable', function(){
-            search_input.val('').keyup();
-        });
-        search_input.after(clear_button).after(' ');
-    });
 });
