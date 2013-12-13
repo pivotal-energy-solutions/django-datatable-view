589e6f16ca6b8ec572d412a8fca1aa354c42f6ae
Tim Valenta <tonightslastsong@gmail.com>
2013-12-12 23:35:21 -0700

    Add Django <1.5 compatibility layer
    Google App Engine prefers 1.4 in its documentation, and ain’t nobody
    got time to figure differently, so 1.4 it is.

---
+++ datatableview/tests/example_project/example_project/example_app/templatetags/example_app_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from django import get_version
+
+register = template.Library()
+
+if get_version().split('.') < ['1', '5']:
+    from django.template.defaulttags import url
+    from django.core.urlresolvers import reverse
+    @register.simple_tag(name="url")
+    def django_1_4_url_simple(url_name):
+        return reverse(url_name)
