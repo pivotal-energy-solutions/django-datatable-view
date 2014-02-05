123973353645b6249b7a3320042641702cc13b90
Tim Valenta <tonightslastsong@gmail.com>
2014-02-05 13:18:48 -0700

    Bump version number to 0.7.0

--- datatableview/__init__.py
+++ datatableview/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __name__ = 'datatableview'
 __author__ = 'Tim Valenta'
-__version_info__ = (0, 6, 5)
+__version_info__ = (0, 7, 0)
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '2013/11/14 2:00:00 PM'
 __credits__ = ['Tim Valenta', 'Steven Klass']
