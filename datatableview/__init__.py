120c9621597b9bedc82ca491c9941011f2d6d0d3
Charlie Hornsby <charlie.hornsby@andersinnovations.com>
2015-01-22 09:53:57 +0200

    Bump version number

--- datatableview/__init__.py
+++ datatableview/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __name__ = 'datatableview'
 __author__ = 'Tim Valenta'
-__version_info__ = (0, 8, 1)
+__version_info__ = (0, 8, 1, 2)
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '2013/11/14 2:00:00 PM'
 __credits__ = ['Tim Valenta', 'Steven Klass']
