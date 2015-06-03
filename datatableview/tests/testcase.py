c15cb22a935b2bea449a390b2958d1b08964917a
Tim Valenta <tonightslastsong@gmail.com>
2015-06-03 13:48:54 -0700

    Add support for django.apps cache in tests

--- datatableview/tests/testcase.py
+++ datatableview/tests/testcase.py
@@ -1,9 +1,19 @@
 # -*- encoding: utf-8 -*-
 
+from django import get_version
 from django.test import TestCase
-from django.test.utils import override_settings
 from django.core.management import call_command
-from django.db.models import loading
+
+if get_version().split('.') >= ['1', '7']:
+    from django.test import override_settings
+    from django.apps import apps
+    clear_app_cache = apps.clear_cache
+else:
+    from django.test.utils import override_settings
+    from django.db.models import loading
+    def clear_app_cache():
+        loading.cache.loaded = False
+
 
 @override_settings(INSTALLED_APPS=[
     'datatableview',
@@ -15,6 +25,6 @@ class DatatableViewTestCase(TestCase):
         """
         Asks the management script to re-sync the database.  Having test-only models is a pain.
         """
-        loading.cache.loaded = False
+        clear_app_cache()
         call_command('syncdb', interactive=False, verbosity=0)
         super(DatatableViewTestCase, self)._pre_setup()
