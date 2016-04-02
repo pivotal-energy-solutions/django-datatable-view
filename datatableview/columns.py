0029a128ec588653440481bcd58d7bc435e386dc
Anna Prochowska <panna.anna101@gmail.com>
2016-04-02 14:12:59 +0200

    Fixed ImportError in Django 1.9

--- datatableview/columns.py
+++ datatableview/columns.py
@@ -15,7 +15,10 @@ from django.db.models.fields import FieldDoesNotExist
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.encoding import smart_text
 from django.utils.safestring import mark_safe
-from django.forms.util import flatatt
+try:
+    from django.forms.util import flatatt
+except ImportError:
+    from django.forms.utils import flatatt
 from django.template.defaultfilters import slugify
 try:
     from django.utils.encoding import python_2_unicode_compatible
