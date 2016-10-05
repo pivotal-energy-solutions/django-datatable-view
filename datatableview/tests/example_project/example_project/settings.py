858b19dca3a7f7173b8399b60b3e02525cc1765d
Tim Valenta <tonightslastsong@gmail.com>
2016-10-04 17:00:28 -0800

    Enable auth and contenttypes for Django 1.10
    There's an issue with Django importing AnonymousUser from auth and thus
    triggering the app loader, which finds User and ContentType, which it
    things haven't been migrated.  So I'll give up on the sweet sweet
    simplicity of having nearly zero installed apps in the demo.

--- datatableview/tests/example_project/example_project/settings.py
+++ datatableview/tests/example_project/example_project/settings.py
@@ -60,8 +60,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     # 'django.contrib.admin',
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     # 'django.contrib.sessions',
     # 'django.contrib.messages',
     'django.contrib.staticfiles',
