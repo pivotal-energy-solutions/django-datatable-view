18f473c9df582e51910760a5bc79ce33bb7cb290
Tim Valenta <tonightslastsong@gmail.com>
2016-10-04 16:57:47 -0800

    Declare Django 1.10 TEMPLATES setting

--- datatableview/tests/example_project/example_project/settings.py
+++ datatableview/tests/example_project/example_project/settings.py
@@ -34,6 +34,24 @@ SECRET_KEY = 'bad secret key'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ]
+        }
+    },
+]
 
 ALLOWED_HOSTS = []
 
