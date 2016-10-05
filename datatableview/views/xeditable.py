f277ccc49245b5fabc43f99b7dc8504c9c754cd7
Tim Valenta <tonightslastsong@gmail.com>
2016-10-05 11:12:02 -0800

    Remove use of deprecated field API

--- datatableview/views/xeditable.py
+++ datatableview/views/xeditable.py
@@ -6,6 +6,7 @@ import operator
 import logging
 
 from ..forms import XEditableUpdateForm
+from ..compat import get_field
 from .base import DatatableView
 
 from django import get_version
@@ -62,7 +63,7 @@ class XEditableMixin(object):
             if field_name not in datatable.config['columns']:
                 return HttpResponseBadRequest("Invalid field name")
 
-        field = self.model._meta.get_field_by_name(field_name)[0]
+        field, _ = get_field(self.model._meta, field_name)
 
         choices = self.get_field_choices(field, field_name)
         return HttpResponse(json.dumps(choices))
