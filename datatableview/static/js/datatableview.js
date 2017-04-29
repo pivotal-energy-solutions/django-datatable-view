79b8e08043e60caa7cfd65e4679b34365862141b
Tim Valenta <tonightslastsong@gmail.com>
2017-04-28 16:14:38 -0800

    Factor out DOM option discovery
    This should enable people to use the option code we've provided even if
    they don't want to use our initialization method.

--- datatableview/static/js/datatableview.js
+++ datatableview/static/js/datatableview.js
@@ -71,73 +71,77 @@ var datatableview = (function(){
 
             $$.each(function(){
                 var datatable = $(this);
-                var columnOptions = [];
-                var sortingOptions = [];
-
-                datatable.find('thead th').each(function(){
-                    var header = $(this);
-                    var options = {};
-                    for (var i = 0; i < header[0].attributes.length; i++) {
-                        var attr = header[0].attributes[i];
-                        if (attr.specified && /^data-/.test(attr.name)) {
-                            var name = attr.name.replace(/^data-/, '');
-                            var value = attr.value;
-
-                            // Typecasting out of string
-                            name = optionsNameMap[name];
-                            if (/^b/.test(name)) {
-                                value = (value === 'true');
-                            }
-
-                            if (name == 'order') {
-                                // This doesn't go in the columnOptions
-                                var sort_info = value.split(',');
-                                sort_info[1] = parseInt(sort_info[1]);
-                                sortingOptions.push(sort_info);
-                                continue;
-                            }
-
-                            options[name] = value;
-                        }
-                    }
-                    columnOptions.push(options);
-                });
-
-                // Arrange the sorting column requests and strip the priority information
-                sortingOptions.sort(function(a, b){ return a[0] - b[0] });
-                for (var i = 0; i < sortingOptions.length; i++) {
-                    sortingOptions[i] = sortingOptions[i].slice(1);
-                }
-
-                options = $.extend({}, datatableview.defaults, opts, {
-                    "order": sortingOptions,
-                    "columns": columnOptions,
-                    "pageLength": datatable.attr('data-page-length'),
-                    "infoCallback": function(oSettings, iStart, iEnd, iMax, iTotal, sPre){
-                        $("#" + datatable.attr('data-result-counter-id')).html(parseInt(iTotal).toLocaleString());
-                        var infoString = oSettings.oLanguage.sInfo.replace('_START_',iStart).replace('_END_',iEnd).replace('_TOTAL_',iTotal);
-                        if (iMax != iTotal) {
-                            infoString += oSettings.oLanguage.sInfoFiltered.replace('_MAX_',iMax);
-                        }
-                        return infoString;
-                    }
-                });
-                options.ajax = $.extend(options.ajax, {
-                    "url": datatable.attr('data-source-url'),
-                    "type": datatable.attr('data-ajax-method') || 'GET',
-                    "beforeSend": function(request){
-                        request.setRequestHeader("X-CSRFToken", datatableview.getCookie('csrftoken'));
-                    }
-                });
-                try {
-                    options = confirm_datatable_options(options, datatable);
-                } catch (e) {
-
-                }
-
+                var options = datatableview.getOptions(datatable);
                 datatable.DataTable(options);
             });
             return $$;
+        }
+
+        getOptions: function(datatable) {
+            var columnOptions = [];
+            var sortingOptions = [];
+
+            datatable.find('thead th').each(function(){
+                var header = $(this);
+                var options = {};
+                for (var i = 0; i < header[0].attributes.length; i++) {
+                    var attr = header[0].attributes[i];
+                    if (attr.specified && /^data-/.test(attr.name)) {
+                        var name = attr.name.replace(/^data-/, '');
+                        var value = attr.value;
+
+                        // Typecasting out of string
+                        name = optionsNameMap[name];
+                        if (/^b/.test(name)) {
+                            value = (value === 'true');
+                        }
+
+                        if (name == 'order') {
+                            // This doesn't go in the columnOptions
+                            var sort_info = value.split(',');
+                            sort_info[1] = parseInt(sort_info[1]);
+                            sortingOptions.push(sort_info);
+                            continue;
+                        }
+
+                        options[name] = value;
+                    }
+                }
+                columnOptions.push(options);
+            });
+
+            // Arrange the sorting column requests and strip the priority information
+            sortingOptions.sort(function(a, b){ return a[0] - b[0] });
+            for (var i = 0; i < sortingOptions.length; i++) {
+                sortingOptions[i] = sortingOptions[i].slice(1);
+            }
+
+            options = $.extend({}, datatableview.defaults, opts, {
+                "order": sortingOptions,
+                "columns": columnOptions,
+                "pageLength": datatable.attr('data-page-length'),
+                "infoCallback": function(oSettings, iStart, iEnd, iMax, iTotal, sPre){
+                    $("#" + datatable.attr('data-result-counter-id')).html(parseInt(iTotal).toLocaleString());
+                    var infoString = oSettings.oLanguage.sInfo.replace('_START_',iStart).replace('_END_',iEnd).replace('_TOTAL_',iTotal);
+                    if (iMax != iTotal) {
+                        infoString += oSettings.oLanguage.sInfoFiltered.replace('_MAX_',iMax);
+                    }
+                    return infoString;
+                }
+            });
+            options.ajax = $.extend(options.ajax, {
+                "url": datatable.attr('data-source-url'),
+                "type": datatable.attr('data-ajax-method') || 'GET',
+                "beforeSend": function(request){
+                    request.setRequestHeader("X-CSRFToken", datatableview.getCookie('csrftoken'));
+                }
+            });
+            try {
+                options = confirm_datatable_options(options, datatable);
+            } catch (e) {
+
+            }
+            return options;
         }
     }
 })();
