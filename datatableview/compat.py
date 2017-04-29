1be202f1b8708052a2f9178df917141f1b3325f7
Tim Valenta <tonightslastsong@gmail.com>
2017-04-28 16:42:22 -0800

    Only use legacy API on 1.7
    There's an issue with the way related fields are returned in 1.7 using
    the newer "get_field()" API, so we don't want to use it even though
    it's available

--- datatableview/compat.py
+++ datatableview/compat.py
@@ -28,15 +28,15 @@ def python_2_unicode_compatible(klass):
     return klass
 
 
-USE_LEGACY_FIELD_API = django.VERSION < (1, 9)
+USE_LEGACY_FIELD_API = django.VERSION < (1, 8)
 
 def get_field(opts, field_name):
     """ Retrieves a field instance from a model opts object according to Django version. """
-    if USE_LEGACY_FIELD_API:
-        field, _, direct, _ = opts.get_field_by_name(field_name)
-    else:
+    if not USE_LEGACY_FIELD_API:
         field = opts.get_field(field_name)
         direct = not field.auto_created
+    else:
+        field, _, direct, _ = opts.get_field_by_name(field_name)
     return field, direct
 
 def escape_uri_path(path):
