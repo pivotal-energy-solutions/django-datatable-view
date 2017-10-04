0a3a83b89bf859f5bacb6d090a44e5df515270f9
Steven Klass <rh0dium@users.noreply.github.com>
2017-10-04 14:28:46 -0700

    Handle case where the field simply no longer exists (impersonate removed the column)

--- datatableview/datatables.py
+++ datatableview/datatables.py
@@ -385,9 +385,13 @@ class Datatable(six.with_metaclass(DatatableMetaclass)):
                 name = name[1:]
 
             if name not in self.columns:
-                field = resolve_orm_path(self.model, name)
-                column = get_column_for_modelfield(field)
-                ordering_columns[name] = column(sources=[name])
+                try:
+                    field = resolve_orm_path(self.model, name)
+                except FieldDoesNotExist:
+                    pass
+                else:
+                    column = get_column_for_modelfield(field)
+                    ordering_columns[name] = column(sources=[name])
 
         return ordering_columns
 
