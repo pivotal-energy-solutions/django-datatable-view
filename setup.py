71fee17bebec908db43170a3b61abc6872412979
Autumn <avalenta@pivotalenergysolutions.com>
2018-10-04 21:42:11 -0700

    Poke updated info into setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(name='django-datatable-view',
                   ' with (http://datatables.net/plug-ins/api).  The package consists of '
                   'a class-based view, and a small collection of utilities for rendering'
                   ' table data from models.',
-      author='Tim Valenta',
-      author_email='tvalenta@pivotalenergysolutions.com',
+      author='Autumn Valenta',
+      author_email='avalenta@pivotalenergysolutions.com',
       url='https://github.com/pivotal-energy-solutions/django-datatable-view',
       download_url='https://github.com/pivotal-energy-solutions/django-datatable-view/tarball/django-datatable-view-0.9.0-beta.5',
       license='Apache License (2.0)',
@@ -28,5 +28,5 @@ setup(name='django-datatable-view',
       packages=find_packages(exclude=['tests', 'tests.*']),
       package_data={'datatableview': ['static/js/*.js', 'templates/datatableview/*.html']},
       include_package_data=True,
-      install_requires=['django>=1.2', 'python-dateutil>=2.1'],
+      install_requires=['django>=1.11', 'python-dateutil>=2.1'],
 )
