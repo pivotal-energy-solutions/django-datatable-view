2afa948003c4898fe15fa711bde1f7670f514baa
Autumn <tonightslastsong@gmail.com>
2018-10-04 21:05:12 -0700

    Modernize

--- datatableview/tests/testcase.py
+++ datatableview/tests/testcase.py
@@ -1,22 +1,9 @@
 # -*- encoding: utf-8 -*-
 
-import django
 from django.test import TestCase
+from django.test import override_settings
 from django.core.management import call_command
-
-if django.VERSION >= (1, 7):
-    from django.test import override_settings
-    from django.apps import apps
-    migrate_command = 'migrate'
-    initial_data_fixture = 'initial_data_modern'
-    clear_app_cache = apps.clear_cache
-else:
-    from django.test.utils import override_settings
-    from django.db.models import loading
-    migrate_command = 'syncdb'
-    initial_data_fixture = 'initial_data_legacy'
-    def clear_app_cache():
-        loading.cache.loaded = False
+from django.apps import apps
 
 
 @override_settings(INSTALLED_APPS=[
@@ -29,7 +16,7 @@ class DatatableViewTestCase(TestCase):
         """
         Asks the management script to re-sync the database.  Having test-only models is a pain.
         """
-        clear_app_cache()
-        call_command(migrate_command, interactive=False, verbosity=0)
-        call_command('loaddata', initial_data_fixture, interactive=False, verbosity=0)
+        apps.clear_cache()
+        call_command('migrate', interactive=False, verbosity=0)
+        call_command('loaddata', 'initial_data', verbosity=0)
         super(DatatableViewTestCase, self)._pre_setup()
