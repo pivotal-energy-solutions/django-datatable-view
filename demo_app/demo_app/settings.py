59822c8d2fcd66f86c53267e3d6de7c7631aab72
Steven K <steven@pointcircle.com>
2022-04-19 16:28:22 -0700

    Unify and simplify for tests.

--- demo_app/demo_app/settings.py
+++ demo_app/demo_app/settings.py
@@ -100,6 +100,12 @@ DATABASES = {
         "PASSWORD": env("MYSQL_PASSWORD"),
         "HOST": env("MYSQL_HOST"),
         "PORT": env("DOCKER_MYSQL_PORT", default=env("MYSQL_PORT", default="3306")),
+        "OPTIONS": {"charset": "utf8mb4"},
+        "TEST": {
+            "MIGRATE": False,
+            "CHARSET": "utf8mb4",
+            "COLLATION": "utf8mb4_unicode_520_ci",
+        },
     }
 }
 
--- demo_app/demo_app/settings_test.py
+++ demo_app/demo_app/settings_test.py
@@ -2,29 +2,22 @@
 
 import warnings
 
-from .settings import *
-
-
-class DisableMigrations(object):
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return None
-
-
-MIGRATION_MODULES = DisableMigrations()
+from .settings import *  # noqa: F403,F401
 
 # Handle system warning as log messages
 warnings.simplefilter("once")
 
 for handler in LOGGING.get("handlers", []):
-    LOGGING["handlers"][handler]["level"] = "ERROR"
+    LOGGING["handlers"][handler]["level"] = "CRITICAL"
 for logger in LOGGING.get("loggers", []):
-    LOGGING["loggers"][logger]["level"] = "ERROR"
+    LOGGING["loggers"][logger]["level"] = "CRITICAL"
 
 mysql_db = DATABASES["default"]
-DEFAULT_DB = {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
+DEFAULT_DB = {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": ":memory:",
+    "TEST": {"MIGRATE": False},
+}
 if os.environ.get("DB_TYPE") == "mysql":
     print("Using MySQL Backend!")
     DEFAULT_DB = mysql_db
